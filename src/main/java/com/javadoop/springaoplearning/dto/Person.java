9845f3f30d6759ef85c7bf2c7a1faab628fcd16b
zhangyu <tingjiang.zy@raycloud.com>
2024-04-07 11:21:11 +0800

    commit

---
+++ src/main/java/com/javadoop/springaoplearning/dto/Person.java
@@ -0,0 +1,99 @@
+package com.javadoop.springaoplearning.dto;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+
+public class Person implements BeanFactoryAware, BeanNameAware, InitializingBean, DisposableBean {
+
+    private String name;
+
+    private String address;
+
+    private int phone;
+
+    private BeanFactory beanFactory;
+
+    private String beanName;
+
+    public Person() {
+        System.out.println("构造器调用Person的构造器实例化");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println("注入属性name");
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        System.out.println("注入属性address");
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        System.out.println("注入属性phone");
+        this.phone = phone;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone=" + phone +
+                ", beanFactory=" + beanFactory +
+                ", beanName='" + beanName + '\'' +
+                '}';
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("【BeanFactoryAware接口】调用BeanFactoryAware.setBeanFactory()");
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("【BeanNameAware接口】调用BeanNameAware.setBeanName()");
+        this.beanName = name;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("【DisposableBean接口】调用DisposableBean.destroy()");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("【InitializingBean接口】调用InitializingBean.afterPropertiesSet()");
+    }
+
+    // 通过<bean>的init-method属性指定的初始化方法
+    public void myInit() {
+        System.out.println("【init-method】调用<bean>的init-method属性指定的初始化方法");
+    }
+
+    // 通过<bean>的destroy-method属性指定的初始化方法
+    public void myDestroy() {
+        System.out.println("【destroy-method】调用<bean>的destroy-method属性指定的初始化方法");
+    }
+}
